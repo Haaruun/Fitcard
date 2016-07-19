4f46b01139611b6956503a76f28e9ae53beb9b64
Haaruun <haaruun.dhubat@outlook.com>
2016-07-18 21:05:55 -0400

    button

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,10 @@
 $(function(){
-	$('.hamburger').click(function(e){
-		e.preventDefault();
+	$('.hamburger').click(function(){
 		$('.ham_one').toggleClass('one');
 		$('.ham_two').toggleClass('two');
 		$('.ham_three').toggleClass('three');
 		$(this).toggleClass('ham');
 		$('.menu').toggleClass('menu_slide');
+
 	});
 });
